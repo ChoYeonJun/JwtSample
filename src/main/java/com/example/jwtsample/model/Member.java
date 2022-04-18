1ca94cad2f4e46d99eb99c4e84e360f5d87c37fa
YeonJun Cho <micayel1219@gmail.com>
2022-04-18 23:47:29 +0900

    jwt sample pj add

---
+++ src/main/java/com/example/jwtsample/model/Member.java
@@ -0,0 +1,38 @@
+package com.example.jwtsample.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    public Member(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public static Member createMember(String email, String password) {
+        return new Member(email,password);
+    }
+
+}
---
+++ src/main/java/com/example/jwtsample/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.example.jwtsample.repository;
+
+import com.example.jwtsample.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
