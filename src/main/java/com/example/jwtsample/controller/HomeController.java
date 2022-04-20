755168eb715cb476ae072f797d9dcac929bd2706
YeonJun Cho <micayel1219@gmail.com>
2022-04-20 20:58:08 +0900

    return hello world with data

--- src/main/java/com/example/jwtsample/controller/HomeController.java
+++ src/main/java/com/example/jwtsample/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.jwtsample.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/hello")
-    public String firstPage() {
-        return "Hello World";
+    public ResponseEntity<Hello> firstPage() {
+        return ResponseEntity.ok(new Hello("Hello world"));
     }
 
+
+}
+@Data
+@AllArgsConstructor
+class Hello {
+    private String message;
 }
